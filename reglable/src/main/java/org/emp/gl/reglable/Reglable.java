b0d401242a33e517001d4d03f583def2e5554794
younesmch <mostafayounesse@gmail.com>
2021-05-17 12:27:48 +0200

    Add files via upload

---
+++ reglable/src/main/java/org/emp/gl/reglable/Reglable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.emp.gl.reglable;
+
+/**
+ *
+ * @author pc
+ */
+public interface Reglable {
+    public void onMode();
+    public void doconfig();
+    public void increment();
+    
+}
